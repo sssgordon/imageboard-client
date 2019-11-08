19826ae764fc82e4d15c7586759ff2868f02ce97
sssgordon <gso7@uwo.ca>
2019-11-08 15:22:54 +0100

    bug fixes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import store from "./store";
 import { Provider } from "react-redux";
 
-class App extends Component() {
+class App extends Component {
   render() {
     return (
       <Provider store={store}>
